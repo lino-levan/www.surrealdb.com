fdfaac8594dae4ea7591c0e56b9688cef6fadd6e
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2022-11-18 18:54:08 -0800

    docs: update deno example

--- app/snippets/docs/integration/libraries/deno/basic.js
+++ app/snippets/docs/integration/libraries/deno/basic.js
@@ -1,50 +1,42 @@
-import Surreal from "https://deno.land/x/surrealdb@v0.2.0/mod.ts";
+import Surreal from "https://deno.land/x/surrealdb@v0.5.0/mod.ts";
 
 const db = new Surreal('http://127.0.0.1:8000/rpc');
 
-async function main() {
+try {
 
-	try {
+	// Signin as a namespace, database, or root user
+	await db.signin({
+		user: 'root',
+		pass: 'root',
+	});
 
-		// Signin as a namespace, database, or root user
-		await db.signin({
-			user: 'root',
-			pass: 'root',
-		});
+	// Select a specific namespace / database
+	await db.use('test', 'test');
 
-		// Select a specific namespace / database
-		await db.use('test', 'test');
+	// Create a new person with a random id
+	let created = await db.create("person", {
+		title: 'Founder & CEO',
+		name: {
+			first: 'Tobie',
+			last: 'Morgan Hitchcock',
+		},
+		marketing: true,
+		identifier: Math.random().toString(36).substr(2, 10),
+	});
 
-		// Create a new person with a random id
-		let created = await db.create("person", {
-			title: 'Founder & CEO',
-			name: {
-				first: 'Tobie',
-				last: 'Morgan Hitchcock',
-			},
-			marketing: true,
-			identifier: Math.random().toString(36).substr(2, 10),
-		});
+	// Update a person record with a specific id
+	let updated = await db.change("person:jaime", {
+		marketing: true,
+	});
 
-		// Update a person record with a specific id
-		let updated = await db.change("person:jaime", {
-			marketing: true,
-		});
+	// Select all people records
+	let people = await db.select("person");
 
-		// Select all people records
-		let people = await db.select("person");
+	// Perform a custom advanced query
+	let groups = await db.query('SELECT marketing, count() FROM type::table($tb) GROUP BY marketing', {
+		tb: 'person',
+	});
 
-		// Perform a custom advanced query
-		let groups = await db.query('SELECT marketing, count() FROM type::table($tb) GROUP BY marketing', {
-			tb: 'person',
-		});
-
-	} catch (e) {
-
-		console.error('ERROR', e);
-
-	}
-
+} catch (e) {
+	console.error('ERROR', e);
 }
-
-main();
